f0a1e41c8fec0d374a1c9a1df6e3f731402fb92b
Dominic Black <me@jdm.black>
2017-01-30 14:31:33 +0000

    Added minecraft image from 2 years ago which I forgot to commit

--- build.sh
+++ build.sh
@@ -3,5 +3,7 @@
 for build in */; do
   build=${build%*/}
 
-  docker build --force-rm=true -t domblack/$build $build
+  if [ -e "$build/Dockerfile"]; then
+    docker build --force-rm=true -t domblack/$build $build
+  fi
 done
